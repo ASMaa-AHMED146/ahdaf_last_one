7ba2b5ece40e3894bd9fc7622ac429434e8f2754
ASMaa-AHMED146 <158354808+ASMaa-AHMED146@users.noreply.github.com>
2025-08-28 12:14:14 +0300

    poordata

--- src/components/sections/packagesoffer.jsx
+++ src/components/sections/packagesoffer.jsx
@@ -1,52 +1,67 @@
+
 import React, { useState } from 'react'
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 import { FaRegCircleCheck } from "react-icons/fa6";
 import { BiPhoneCall } from "react-icons/bi";
 import { BsWhatsapp } from "react-icons/bs";
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Service() {
-    const [showservices,setshowservices]=useState(true)
-  return (
-    <div>
-      <div className='bg-[#1A74740A] flex justify-between px-[10px] py-[15px] items-center rounded-[10px]'>
-        <p className='text-[#F36C35] font-[700] text-[32px]'>الخدمات التي توفرها الباقة</p>
-       {
-         showservices? <IoIosArrowDown className='text-[25px]' style={{color:'#F36C35'}} onClick={()=>setshowservices(!showservices)} />: <IoIosArrowUp className='text-[25px]' style={{color:'#F36C35'}} onClick={()=>setshowservices(!showservices)}/>
-       }
-        </div> 
-        {
-            showservices&&(
-                <>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                </>
-            )
-        }
-       <div className='flex gap-[12px]'>
-         <button className='flex items-center gap-[7px] text-[#1A7474]  border-[1px] border-[#1A7474] py-[7px] px-[30px] rounded-[10px] mt-[50px] cursor-pointer'>
-            <BiPhoneCall style={{color:'#1A7474'}}/>اتصل بنا 
-        </button>
-        <button className='flex items-center bg-[#45B224] gap-[7px] text-[#FAFAFA] border-[1px]  py-[7px] px-[30px] rounded-[10px] mt-[50px] cursor-pointer'>
-             <BsWhatsapp style={{color:'#FAFAFA'}}/>اتصل بنا 
-        </button>
-       </div>
-       <div className='my-[30px] text-center '>
-         <button className='border-[1px] border-[#F36C35] text-[#F36C35] rounded-[5px] w-[50%] py-[10px] cursor-pointer'>احجز الان</button>
-       </div>
-    </div>
-  )
+    const [showservices, setshowservices] = useState(true);
+    const nav=useNavigate();
+
+    function whatscall() {
+        const phoneNumber = "201098757146"; 
+        const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
+        window.open(whatsappUrl, "_blank");
+    }
+    
+    function phonecall() {
+        const phoneNumber = "01098757146";
+        window.open(`tel:${phoneNumber}`, "_self");
+    }
+    
+    return (
+        <div>
+            <div className='bg-[#1A74740A] flex justify-between px-[10px] py-[15px] items-center rounded-[10px]'>
+                <p className='text-[#F36C35] font-[700] text-[32px]'>الخدمات التي توفرها الباقة</p>
+                {
+                    showservices ? 
+                    <IoIosArrowDown className='text-[25px]' style={{color:'#F36C35'}} onClick={() => setshowservices(!showservices)} /> : 
+                    <IoIosArrowUp className='text-[25px]' style={{color:'#F36C35'}} onClick={() => setshowservices(!showservices)} />
+                }
+            </div> 
+            {
+                showservices && (
+                    <>
+                        <Item />
+                        <Item />
+                        <Item />
+                        <Item />
+                    </>
+                )
+            }
+            <div className='flex gap-[12px]'>
+                <button onClick={() => phonecall()} className='flex items-center gap-[7px] text-[#1A7474] border-[1px] border-[#1A7474] py-[7px] px-[30px] rounded-[10px] mt-[50px] cursor-pointer'>
+                    <BiPhoneCall style={{color:'#1A7474'}} />اتصل بنا 
+                </button>
+                <button onClick={() => whatscall()} className='flex items-center bg-[#45B224] gap-[7px] text-[#FAFAFA] border-[1px] py-[7px] px-[30px] rounded-[10px] mt-[50px] cursor-pointer'>
+                    <BsWhatsapp style={{color:'#FAFAFA'}} />اتصل بنا 
+                </button>
+            </div>
+            <div className='my-[30px] text-center'>
+                <button onClick={()=>nav('/booking')} className='border-[1px] border-[#F36C35] text-[#F36C35] rounded-[5px] w-[50%] py-[10px] cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110 active:scale-95'>احجز الان</button>
+            </div>
+        </div>
+    )
 }
 
-function Item ()
-{
-    return(
+function Item() {
+    return (
         <>
-            <div className='flex gap-[18px] mt-[30px] '> 
-                <FaRegCircleCheck className='text-[45px]' style={{color:'#F36C35'}}/>
+            <div className='flex gap-[18px] mt-[30px]'> 
+                <FaRegCircleCheck className='text-[45px]' style={{color:'#F36C35'}} />
                 <p className='font-[400] text-[24px] text-[#707070]'>خدمات إصلاح كهربائية احترافية لجميع احتياجات منزلك. نتعامل مع كل شيء من إصلاح المنافذ والمفاتيح إلى استكشاف المشكلات الكهربائية المعقدة وإصلاحها، مما يضمن عمل الأنظمة الكهربائية في منزلك بأمان وكفاءة.</p>
             </div>
         </>
--- src/pages/Packagesdetails.jsx
+++ src/pages/Packagesdetails.jsx
@@ -7,6 +7,8 @@ import LottieHandler from '../components/feedback/LottieHandler/LottieHandler';
 import CircularProgress from '@mui/material/CircularProgress';
 import Heading from '../components/common/Heading/Headind';
 import Packageservice from "../components/sections/packagesoffer"
+import Commonfaq from "../components/sections/commonfaq"
+import CommonSlider from '../components/sections/CommonSlider';
 
 export default function Packagesdetails() {
   const { id } = useParams();
@@ -17,11 +19,11 @@ export default function Packagesdetails() {
 
   const {items,error,loading}=useSelector((state)=>state.package);
   const data=items?.packages || [];
+  console.log(data);
+  
   const selecteditem=data.filter((el)=>(
     el.id==id
   )) 
-  console.log(items);
-  
   if(loading==='pending')
     {
       return <div className='h-[350px] w-full flex items-center justify-center flex-col gap-[20px]'>
@@ -39,21 +41,22 @@ export default function Packagesdetails() {
     <div>
       <Heading service={'الباقات'} sub={selecteditem[0]?.title_ar|| 'خدمة فرعية'}/>
        <div className='px-[150px] py-[50px]  '>
-                <div>
+                 {
+        selecteditem?(
+          <div>
                   <img src={selecteditem[0]?.image} alt="" className='rounded-[20px] h-[450px] w-full' />
                   <div className='p-[20px]'>
                     <p className='font-[500] text-[24px] '>{selecteditem[0]?.title_ar|| 'خدمة فرعية'}</p>
                     {/* <p className='text-[#707070] font-[500] mt-[12px]'>باقة شاملة للتصليح والصيانة لضمان أفضل أداء لممتلكاتك</p> */}
                   </div>
                 </div>
-                <Packageservice/>
+        ):<LottieHandler type={"empty"}/>
+      } 
+      <Packageservice/>
+      <Commonfaq/>
               </div>
 
-      {/* {
-        selecteditem?(
-
-        ):<LottieHandler type={"empty"}/>
-      } */}
+      <CommonSlider array={data}/>
     </div>
   )
 }
